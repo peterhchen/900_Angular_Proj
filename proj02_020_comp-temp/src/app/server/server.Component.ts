ba62f70df78394a6b356cf18b2e2b357c44330e2
peterhchen <peter.hp.chen@gmail.com>
2020-08-04 20:27:00 -0700

    Add files via upload

---
+++ proj02_020_comp-temp/src/app/server/server.Component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.Component.html'
+})
+
+export class ServerComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}

27f81c081ba4435e33a8dcaf69fd19437d896460
peterhchen <peter.hp.chen@gmail.com>
2020-08-04 21:15:56 -0700

    Add files via upload

---
+++ proj05_083_assignment04_bind-prop-event/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  oddNumbers: number [] = [];
+  evenNumbers: number [] = [];
+  onIntervalFired(fireNumber: number) {
+    console.log('fireNumber: ', fireNumber)
+    if (fireNumber % 2 === 0) {
+      this.evenNumbers.push (fireNumber)
+    } else {
+      this.oddNumbers.push (fireNumber)
+    }
+  }
+}

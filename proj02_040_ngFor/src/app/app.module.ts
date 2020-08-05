00211141cdd58156bb0c33c8be884f8f0383c329
peterhchen <peter.hp.chen@gmail.com>
2020-08-04 20:44:53 -0700

    Add files via upload

---
+++ proj02_040_ngFor/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ServerComponent } from './server/server.Component';
+import { ServersComponent } from './servers/servers.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ proj02_040_ngFor/src/app/servers/servers.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  serverCreated = false;
+  servers=['Testserver 1', 'Testserver 2']
+
+  constructor() { 
+    setTimeout (() => {
+       this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer () {
+    this.serverCreated = true;
+    this.servers.push (this.serverName)
+    this.serverCreationStatus = 'Server was created ' + this.serverName + '!';
+  }
+}

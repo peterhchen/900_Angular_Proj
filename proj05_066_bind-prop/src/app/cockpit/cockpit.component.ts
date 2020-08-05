7931903afbc2a48db3aa6ffc596b6e2fb92b158f
peterhchen <peter.hp.chen@gmail.com>
2020-08-04 21:05:57 -0700

    Add files via upload

---
+++ proj05_066_bind-prop/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer() {
+    console.log ('onAddServer():')
+    // this.serverElements.push({
+    //   type: 'server',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  }
+
+  onAddBlueprint() {
+    console.log ('onAddBluePrint():');
+    // this.serverElements.push({
+    //   type: 'blueprint',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  }
+}

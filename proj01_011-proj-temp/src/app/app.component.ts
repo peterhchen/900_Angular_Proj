0ec316dc586f97f0405ac0b4297301ab34d6daf2
peterhchen <peter.hp.chen@gmail.com>
2020-08-04 20:13:01 -0700

    Add files via upload

---
+++ proj01_011-proj-temp/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'a06-setup';
+}
+
+// export class AppComponent {
+//   title = 'a06-setup';
+// }
